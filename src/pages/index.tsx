6eb4fb4760118367978979831a16f0c09e33c4cc
hrishikeshmane <hrishi.mane26@gmail.com>
2023-09-16 14:27:27 -0400

    fix: description meta tag for LinkedIn

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,16 +13,16 @@ export default function Home(): JSX.Element {
         <title>Hrishikesh Mane | Home</title>
         <link rel="icon" href="/favicon.ico" />
         <meta property="og:title" content="Hrishikesh Mane | Home" />
-        <meta
+        {/* <meta
           property="og:image"
           content="https://avatars.githubusercontent.com/hrishikeshmane"
         />
-        {/* <meta
+        <meta
           name="og:description"
           content="Hrishikesh Mane portfolio website"
-        /> */}
-        <meta property="og:url" content="https://ihrishi.vercel.app/" />
-        <meta property="og:type" content="website" />
+        />
+        <meta property="og:url" content="https://ihrishi.vercel.app/" /> */}
+        {/* <meta property="og:type" content="website" /> */}
       </Head>
 
       <Wrapper>
